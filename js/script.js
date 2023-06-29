9747e569f9792bc4ac040f66c44583240cd267bf
Magdalena Kosowska <magdakosowska32@gmail.com>
2023-06-29 21:13:39 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -5,17 +5,43 @@
 
     for (const task of tasks) {
       htmlString += `
-         <li>
-           ${task.content}
-          </li>
-       `;
+        <li class="tasks__item">
+          <button class="tasks__button tasks__button--toggleDone js-done">
+            ${task.done ? "✔" : ""}
+          </button>
+      `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskItem = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskItem.value.trim();
+
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent);
+      newTaskItem.value = "";
+    }
+
+    newTaskItem.focus();
+  };
+
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
